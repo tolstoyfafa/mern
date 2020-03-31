dfcf7c7ff94f464e946fd91c0bf54f591ec14a84
Fayçal BORDJAH <fbordjah@linagora.com>
2020-03-31 19:11:45 +0200

    Add jwt authentication

--- backend/models/person.js
+++ backend/models/person.js
@@ -1,7 +1,12 @@
 import { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const personSc = new Schema({
-  name: {
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
     type: String,
     required: true
   },
@@ -15,4 +20,24 @@ const personSc = new Schema({
   }
 });
 
+personSc.pre('save', function (next) {
+  var person = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!person.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password along with our new salt
+    bcrypt.hash(person.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      person.password = hash;
+      next();
+    });
+  });
+});
 export default personSc;
